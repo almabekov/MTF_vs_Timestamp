fe99d1aa35728b10c767c075e6f777f86ea01c65
Andrey Almabekov <andrey.almabekov@dal.ca>
2018-11-16 15:10:35 -0400

    Timestamp determenistic algorithm in progress
    Debugging is required a lot

--- src/Implementation.java
+++ src/Implementation.java
@@ -1,3 +1,5 @@
+import java.sql.Time;
+
 public class Implementation {
 
     public static void main(String[] args) {
@@ -9,8 +11,17 @@ public class Implementation {
         ListNode l = ListNode.generateList(10);
         ListNode.printList(l);
 
+        /*
         ReturnValues rv = RequestSequence.serveQueueMTF(rs, l);
         System.out.println(rv.counter); //if we have 100 elements the average access is (10*11)/2, my response was 45, -1 less somehow
         ListNode.printList(rv.head);
+        */
+
+        Timestamp.GetElement(l,0,1);
+        ListNode.printList(l);
+        Timestamp.GetElement(l,9,2);
+        ListNode.printList(l);
+        //Timestamp.GetElement(l,9,3);
+        //ListNode.printList(l);
     }
 }
---
+++ src/Timestamp.java
@@ -0,0 +1,73 @@
+public class Timestamp {
+    //methods to get the element using the MTF list update algorithm
+
+    //get the element elementValue from the list l.
+    //method return number of comparisons during the traversal of the list till necessary element was found
+    // 1 if it was the first element in the list
+    // until n+1 if element is not found
+    //we also put the elment that was found to the head of the list
+    //sequence is the requested element position in the sequence (to remember when that element was requested last time)
+    public static ReturnValues GetElement(ListNode l, int elementValue, int sequence) {
+        ReturnValues rv = new ReturnValues();
+        rv.head=null;
+        rv.counter=0;
+
+        if (l==null) return rv;
+        rv.counter=1;
+        rv.head=l;
+        ListNode oldhead=l;
+        //if our element already is the head
+        //we only update sequence and return the same list;
+        if (l.getValue()==elementValue) {
+            System.out.println("Head element accessed");
+            l.setLastAccess(sequence);
+            return rv;
+        }
+
+        rv.counter++;
+        //otherwise we search for the necessary element
+        while (l.next!=null && l.next.getValue()!=elementValue) {
+            rv.counter++;
+            l=l.next;
+        }
+
+        ListNode previousElement=l;
+        ListNode ourElement=l.next;
+
+        //if element was found in the list
+        if (ourElement!=null && ourElement.getValue()==elementValue) {
+            int lastAccess=ourElement.getLastAccess();
+
+
+
+            //we should swap element only if it was requested at least once before
+            //we need to search from the beginning where we should paste our element
+            if (ourElement.getLastAccess()!=-1) {
+                System.out.println("element with last time -1 accessed");
+                ListNode tempHead = oldhead;
+                //if we should put requested element as a new head
+                if (tempHead.getLastAccess() < lastAccess) {
+                    rv.head = ourElement;
+                    previousElement.next=ourElement.next;
+                    ourElement.next=oldhead;
+                    return rv;
+                } else {
+                    System.out.println("Element that is not head and with last access not -1 accessed");
+                    //search for the closest to the head element, that was accessed before the current element was accessed last time
+                    while (tempHead.next != null && tempHead.next.getLastAccess() > lastAccess) {
+                        tempHead = tempHead.next;
+                    }
+
+                    //if we found such element between head and our element it is tempHead.next
+
+                    if (tempHead.next != null) {
+                        ourElement.next = tempHead.next;
+                        tempHead.next = ourElement;
+                    }
+                }
+            }
+            ourElement.setLastAccess(sequence);
+        }
+        return rv;
+    }
+}
